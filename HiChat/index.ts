a8e5b7095c1dc515b2bcf793488f140950d20742
ROtem <rotemsaadi@gmail.com>
2024-03-01 22:51:42 +0200

    Configure typescript, finished implemente poc for rest

--- HiChat/index.ts
+++ HiChat/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import { RoutesConfiguration } from "./src/configuration/RoutesConfiguration";
 import { router as MessagesRoute } from "./src/routes/MessagesRoute";
 
-const app = express();
+const app: Application = express();
 app.use(cors());
 app.use(RoutesConfiguration.url, MessagesRoute);
 
-app.listen(() => {
+app.listen(RoutesConfiguration.port, () => {
   console.log(`Server listening on port ${RoutesConfiguration.port}`);
 });
